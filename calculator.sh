2f0dac84ef2fc4a9b1500d8883e414c5c9ddfde5
Oreoluwa <rebeccaore.ro@gmail.com>
2026-01-02 18:55:04 +0100

    added a script to prompt user and show disk space

---
+++ calculator.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################### MRMORY HEALTH CHECK SCRIPT  ####################
+########### AUTHOR: OREOLUWA OLUSINDE ######################
+echo "Hello $USER, Welcome."
+sleep 3
+echo "Do you want to check your memory health?"
+read -p "Answer yes or no: " ANSWER
+if [[ ${ANSWER,,}  =~ ^y(es)?$ ]]; then 
+echo "proceeding to display the memory..."
+sleep 2
+echo "loading, please hold on..."
+df -h
+else 
+ echo "NO problem, bye"
+fi
